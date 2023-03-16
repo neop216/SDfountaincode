b334b133473b582dd046a54b1bfb555ee0fb620d
neop216 <neop216@gmail.com>
2023-03-15 23:55:07 -0400

    Split into encode and decode scripts for use with HDTN. Changed file encoding output to be a compressed file per bundle instead of one huge file.

---
+++ decode.py
@@ -0,0 +1,132 @@
+import os
+import math
+import numpy as np
+import sys
+import json
+import gzip
+
+"""
+RATIONALE: A fountain code is a type of encoding process that allows the original data to be recovered from sufficiently
+large subsets of the encoded data. This property makes them highly desirable in hostile network environments, and thus
+perfect for use in space.
+
+https://en.wikipedia.org/wiki/Fountain_code
+
+Arguably the most easily understandable method of encoding blocks involves combining them with the bitwise exclusive-or
+operation. This is the foundation of a subclass of fountain codes called Luby transform (LT) codes. The exact methods of
+encoding and decoding used by LT codes are fairly standardized and agreed upon, and this software prototype attempts to
+implement those methods.
+
+https://en.wikipedia.org/wiki/Luby_transform_code
+
+More advanced and efficient subclasses of fountain code exist, such as Raptor codes, but as most of our research has
+been focused on LT fountain codes, the prototype software below uses their methodology instead.
+
+Implementations studied during research for this project those by:
+    Anson Rosenthal (anrosent): https://github.com/anrosent/LT-Code
+    Darren (darrenldl): https://github.com/darrenldl/ocaml-lt-code
+    Spriteware: https://github.com/Spriteware/lt-codes-python
+    Roberto Francescon (obolo) and Dominik Danelski (Etua): https://github.com/obolo/freeRaptor
+    Daniel Chang (mwdchang): https://github.com/mwdchang/fountain-code
+    Aman Tiwari: https://observablehq.com/@aman-tiwari/fountain-codes
+
+TODO: ARM-Based weirdness? Also introduction of arguments would be nice.
+"""
+
+BUNDLE_BYTES = 1000  # Number of bytes per bundle created from the original data. Must be a power of 2 greater than 8.
+REDUNDANCY = 2  # Scalar for the encoded data's size
+TRANSMISSION_LOSS_PERCENTAGE = 20
+
+
+def decode(encoded_data, original_size):
+    # First, initialize the decoded_data list as a list of -1s with length equal to the number of original bundles.
+    # Since we XORed unsigned integers together, the values of encoded blocks should NEVER be negative. Thus,
+    # initializing the decoded_data list with -1s gives an easy way to check whether an original bundle has been solved.
+    decoded_data = original_size * [-1]
+
+    # Iterate through the encoded_data. If the current encoded bundle has 1 component left, it does not need to wait for
+    # any other bundles to be processed before it can be considered solved. If the decoded_data bundle we're looking at
+    # is unsolved, its value is added to the decoded_data at the appropriate index. Afterwards, we iterate through the
+    # rest of the encoded_data bundles to see if any of them used the solved decoded_data as a component. Upon finding
+    # such a bundle, we can't assign any new decoded_data values, but we can update the bundle's value by XORing it with
+    # the newly-decoded bundle's value. The component can then be considered to have been "removed" from the encoded
+    # bundle, so we remove it from the bundle's component list.
+
+    solved = -1  # Python doesn't have "do-while", so we need to initialize this solved variable here. Can't use "None".
+    while solved != 0:
+        solved = 0
+        for i, bundle in enumerate(encoded_data):
+            if len(bundle["components"]) == 1:
+                solved += 1
+                component_index = bundle["components"][0]
+                encoded_data.pop(i)
+                cur_decode = decoded_data[component_index]
+
+                # Encoding results in some elements of decoded_data being ints and others being numpy arrays. Here, we
+                # would only need the sum of the numpy array as a way to generate an integer to represent it. We're
+                # just checking to make sure that the decoded_data entry we're looking at isn't already solved for
+                # efficiency purposes.
+                if not isinstance(cur_decode, int):
+                    cur_decode = sum(cur_decode)
+
+                if cur_decode < 0:
+                    cur_value = bundle["value"]
+                    decoded_data[component_index] = cur_value
+                    solved += 1
+
+                    for other_bundle in encoded_data:
+                        if len(other_bundle["components"]) > 1 and component_index in other_bundle["components"]:
+                            other_bundle["value"] = cur_value ^ other_bundle["value"]
+                            other_bundle["components"].remove(component_index)
+
+    return decoded_data
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("usage: decode.py (directory)")
+        exit(-1)
+
+    if not os.path.exists(sys.argv[1]):
+        print(f"directory {sys.argv[1]} does not exist")
+        exit(1)
+
+    # Using a text file as input instead of a numpy array because LT code implementation doesn't seem to play nicely
+    # with the numpy arrays of random integers. Using files seems to be a popular method of simulating a data stream
+    # into the LT code software, as was seen in many implementations studied during our research. Also, opening the file
+    # in binary mode allows it to be compiled into a bytearray later, so we use the b mode.
+    # Only really need to get the file extension in this simulated environment, data can be sent without extensions in
+    # practice
+    data = []
+
+    # Read the text file into bundles of predefined size specified by BUNDLE_BYTES above
+    for filename in os.listdir(sys.argv[1]):
+        with gzip.open(sys.argv[1] + "/" + filename, "rb") as f:
+            bundle = f.read()
+
+        # Json library convert string dictionary to real dictionary type.
+        # Double quotes is standard format for json
+        bundle = json.loads(bundle)
+
+        bundle["value"] = np.array(bundle["value"], dtype=np.uint64)
+        data.append(bundle)
+
+    decoded_data = decode(data, round(len(data) / REDUNDANCY))
+    # print(f"\n\n\nDECODED DATA: \n{decoded_data}")
+
+    # To more easily show that decoding was successful, recompile the decoded_data bundles into an output file.
+
+    with open("temp_outfile", "wb") as f:
+        for i, bundle in enumerate(decoded_data):
+            if i < len(decoded_data):
+                f.write(bundle)
+
+    # Remember that we padded the end of our original file with zeros.
+
+    with open("outfile.txt", "wb") as output_file:
+        with open("temp_outfile", "rb") as f:
+            output_file.write(f.read().rstrip(b'\0'))
+    os.remove("temp_outfile")
+
+if __name__ == "__main__":
+    main()
---
+++ encode.py
@@ -0,0 +1,164 @@
+import os
+import random
+import math
+import numpy as np
+import sys
+import gzip
+import shutil
+import json
+
+"""
+RATIONALE: A fountain code is a type of encoding process that allows the original data to be recovered from sufficiently
+large subsets of the encoded data. This property makes them highly desirable in hostile network environments, and thus
+perfect for use in space.
+
+https://en.wikipedia.org/wiki/Fountain_code
+
+Arguably the most easily understandable method of encoding blocks involves combining them with the bitwise exclusive-or
+operation. This is the foundation of a subclass of fountain codes called Luby transform (LT) codes. The exact methods of
+encoding and decoding used by LT codes are fairly standardized and agreed upon, and this software prototype attempts to
+implement those methods.
+
+https://en.wikipedia.org/wiki/Luby_transform_code
+
+More advanced and efficient subclasses of fountain code exist, such as Raptor codes, but as most of our research has
+been focused on LT fountain codes, the prototype software below uses their methodology instead.
+
+Implementations studied during research for this project those by:
+    Anson Rosenthal (anrosent): https://github.com/anrosent/LT-Code
+    Darren (darrenldl): https://github.com/darrenldl/ocaml-lt-code
+    Spriteware: https://github.com/Spriteware/lt-codes-python
+    Roberto Francescon (obolo) and Dominik Danelski (Etua): https://github.com/obolo/freeRaptor
+    Daniel Chang (mwdchang): https://github.com/mwdchang/fountain-code
+    Aman Tiwari: https://observablehq.com/@aman-tiwari/fountain-codes
+
+TODO: ARM-Based weirdness? Also introduction of arguments would be nice.
+"""
+
+BUNDLE_BYTES = 1000  # Number of bytes per bundle created from the original data. Must be a power of 2 greater than 8.
+REDUNDANCY = 2  # Scalar for the encoded data's size
+TRANSMISSION_LOSS_PERCENTAGE = 20
+
+
+def ideal_soliton(k):
+    # The soliton probability distributions are designed to account for transmission errors by intelligently introducing
+    # redundancy. Michael Luby, the namesake of Luby transform (LT) codes, is also the mastermind behind this algorithm.
+    # A more robust version of this distribution exists and could be implemented later.
+    # https://en.wikipedia.org/wiki/Erasure_code
+    # https://en.wikipedia.org/wiki/Soliton_distribution
+    dist = [0, 1 / k]
+    for i in range(2, k + 1):
+        dist.append(1 / (i * (i - 1)))
+    return dist
+
+
+def encode(bundles, original_size, encoded_size):
+    # Start by obtaining an ideal soliton probability distribution that will be used to generate xor neighbor values
+    # later.
+    ideal_dist = ideal_soliton(original_size)
+    xor_possibilities = []
+
+    for i in range(0, original_size + 1):
+        xor_possibilities.append(i)
+
+    # Encode data by cycling through our bundles and XORing them together to create encoded bundles. These bundles
+    # consist of an index number "index", the XORing result "value", an empty list of "components" to be used later,
+    # and the block's number of XOR neighbors "to_solve".
+
+    encoded_data = []
+
+    for i in range(encoded_size):
+        # Randomly choosing a number of xor neighbors, even from a probability distribution, will likely lead to
+        # unsolvable encoding, so to ensure that the encoding is solvable, we start by creating a bundle with one xor
+        # neighbor. In this way, the xor neighbors distribution is slightly less ideal, but it is far more important
+        # that the encoded data is solvable.
+        if i == 0:
+            cur_xor_neighbors = 1
+        else:
+            cur_xor_neighbors = random.choices(xor_possibilities, ideal_dist)[0]
+
+        # This is a FANTASTIC way to avoid having to transport the lists of components used to create each block!
+        # Random.seed(i) will lead the random.sample to ALWAYS select the same "random" numbers from the range desired
+        # in the same order for any given seed i. If we assign each encoded block a seed, we can reverse the process
+        # during decoding by using the same random seeds! Idea discovered in ObservableHQ article "Fountain Codes" by
+        # Aman Tiwari. Python does not have a method of randomly generating seeds other than using the unix timestamp,
+        # which is still too slow to avoid duplicate seeds, so we simply use our loop index i.
+        # https://observablehq.com/@aman-tiwari/fountain-codes
+        random.seed(i)
+        components = random.sample(range(original_size), cur_xor_neighbors)
+
+        cur_encode = bundles[components[0]]
+        for j in range(1, cur_xor_neighbors):
+            cur_encode = cur_encode ^ bundles[components[j]]
+
+        encoded_data.append(dict(value=cur_encode, components=components))
+
+    return encoded_data
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("usage: encode.py (infile)")
+        exit()
+
+    try:
+        input_file = open(sys.argv[1], "rb")
+    except Exception as e:
+        print(e)
+        exit()
+
+    # Using a text file as input instead of a numpy array because LT code implementation doesn't seem to play nicely
+    # with the numpy arrays of random integers. Using files seems to be a popular method of simulating a data stream
+    # into the LT code software, as was seen in many implementations studied during our research. Also, opening the file
+    # in binary mode allows it to be compiled into a bytearray later, so we use the b mode.
+    input_file_name = input_file.name
+    # Only really need to get the file extension in this simulated environment, data can be sent without extensions in
+    # practice
+    _, extension = os.path.splitext(os.path.abspath(input_file.name))
+    data = []
+
+    # Read the text file into bundles of predefined size specified by BUNDLE_BYTES above
+    for i in range(math.ceil(os.path.getsize(input_file_name) / BUNDLE_BYTES)):
+        byte_array = input_file.read(BUNDLE_BYTES)
+        # Testfile is transformed into an object of type bytearray so that it can be parsed by np.frombuffer later
+        byte_array = bytearray(byte_array)
+        byte_array_length = len(byte_array)
+
+        # Just in case our text file does not have an exact multiple of BUNDLE_BYTES bytes (it probably doesn't) pad the
+        # end of the last byte with zeros. This slightly increases the size of the original data, but it is necessary to
+        # make it play nicely with the encoding and decoding process of LT code
+        if byte_array_length < BUNDLE_BYTES:
+            byte_array += bytearray(BUNDLE_BYTES - byte_array_length)
+
+        # np.frombuffer takes our bytearray and interprets it as a buffer of elements of a specific type. The default
+        # value for dtype is float, but since LT code uses XORs, it would be better to interpret these elements as
+        # integers. The magic of how exactly these values are translated to unsigned 64 bit integers is handled by
+        # numpy. If we want to forgo using any python packages (helpful if someone ever translates this to C), we will
+        # need to uncover said magic methods.
+        byte_array = np.frombuffer(byte_array, dtype=np.uint64)  # Also, maybe use np.uint32 if x86 systems? Ask Rachel
+        data.append(byte_array)
+
+    input_file.close()
+
+    # For debugging purposes, we can output all our data sets. Could be added to a verbose option in the future.
+    #print(f"ORIGINAL DATA: \n{data}")
+    encoded_data = encode(data, len(data), round(REDUNDANCY * len(data)))  # Redundancy is introduce            d here
+    #print(f"\n\n\nENCODED DATA: \n{encoded_data}")
+
+    for filename in os.listdir("encodefiles"):
+        os.remove("encodefiles/" + filename)
+
+    for i, bundle in enumerate(encoded_data):
+        if i < len(encoded_data):
+            bundle["value"] = bundle["value"].tolist()
+            to_write = json.dumps(bundle).encode()
+            with open("temp_encodefile", "wb") as f:
+                f.write(to_write)
+            with open("temp_encodefile", "rb") as uncompressed_file:
+                with gzip.open("encodefiles/encodefile_" + str(i) + ".gz", "wb") as compressed_file:
+                    shutil.copyfileobj(uncompressed_file, compressed_file)
+            os.remove("temp_encodefile")
+
+
+if __name__ == "__main__":
+    main()
